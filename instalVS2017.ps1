63dc07bda2c424c1a064fe6be7fa31752c1b9d26
Drew Scoggins <anscoggi@microsoft.com>
2018-10-23 10:15:56 -0700

    Add VS Install script

---
+++ instalVS2017.ps1
@@ -0,0 +1,3 @@
+curl "https://download.visualstudio.microsoft.com/download/pr/5c581991-ecd1-47c1-bba7-f2771a918b9f/b82497e3aea77e45f63388a0bf87e57a/vs_community.exe" -OutFile vs.exe
+
+.\vs.exe --passive --add Microsoft.VisualStudio.Workload.NativeDesktop
